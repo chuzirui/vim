c5cab3fdac6a3808ccef01eab096c7e086518182
Leo Chu <chul@vmware.com>
2017-04-12 20:37:17 -0700

    add some installation

--- checkout.sh
+++ checkout.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+ssh-keygen -t rsa
+apt install -y cscope xsel 
+pip install flake8 tox
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
 cd nsx-ujo
 find . -name "*.py" >> cscope.files
