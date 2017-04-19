14ac6c2f2baf264ed59bbb3a7ea63abfb9d9c634
Leo chu <chuzirui@gmail.com>
2017-04-19 07:15:23 -0400

    askq

--- instk8s.sh
+++ instk8s.sh
@@ -22,20 +22,26 @@ service docker restart
 systemctl enable docker.service
 kubeadm init
 sed -i 's/insecure-port=0/insecure-port=8080/1' /etc/kubernetes/manifests/kube-apiserver.yaml
+
 sed -i "/insecure-port/a\    - --insecure-bind-address=0.0.0.0" /etc/kubernetes/manifests/kube-apiserver.yaml
+read -e -p 'Enter Your NSX IP:'  IP
+read -e -p 'Enter Your NSX account:' -i 'admin' USER
+read -e -p 'Enter Your NSX pwd:' -i 'Admin!23Admin' PWD
+read -e -p 'Enter Your cluster name:'  CLUSTER
+
 cat <<EOF > /tmp/ncp.ini
 [DEFAULT]
 [coe]
-cluster = $4
+cluster = $CLUSTER
 [k8s]
 apiserver_host_ip = 127.0.0.1
 apiserver_host_port = 8080
 use_https = False
 ingress_mode = nat
 [nsx_v3]
-nsx_api_user = $2
-nsx_api_password = $3
-nsx_api_managers = $1:443
+nsx_api_user = $USER
+nsx_api_password = $PWD
+nsx_api_managers = $IP:443
 insecure = True
 subnet_prefix = 24
 external_ip_pool_id = acfe5a81-29eb-4b4a-877b-cae2a6794027
