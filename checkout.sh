4b3862a138979a370c2790b5058f24a8c1672124
Leo <chul@vmware.com>
2017-04-19 19:21:40 -0700

    add some vim stuff

--- checkout.sh
+++ checkout.sh
@@ -3,6 +3,8 @@ ssh-keygen -t rsa
 apt install -y cscope xsel 
 pip install flake8 tox
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
+cp -r .vim/ ~/
+cp .vimrc ~/
 cd nsx-ujo
 find . -name "*.py" >> cscope.files
 cscope -Rk
