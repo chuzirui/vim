7bad889e4807dc05d4b90979a97e12d3090a2233
Leo <chul@vmware.com>
2017-04-21 03:32:59 -0700

    vundle

--- checkout.sh
+++ checkout.sh
@@ -3,8 +3,12 @@ ssh-keygen -t rsa
 apt install -y cscope xsel 
 pip install flake8 tox
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
+git config --global user.email "chul@vmware.com"
+git config --global user.name "Leo"
+
 cp -r .vim/ ~/
 cp .vimrc ~/
+ [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd nsx-ujo
 find . -name "*.py" >> cscope.files
 cscope -Rk
