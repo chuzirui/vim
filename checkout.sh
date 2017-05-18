4358e644cddfa46d74abcaed162e81866e4a3d0f
Leo <chul@vmware.com>
2017-05-17 20:17:14 -0700

    python auto

--- checkout.sh
+++ checkout.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ssh-keygen -t rsa
 apt install -y vim cscope xsel 
-declare -x EDITOR='vim'
+export EDITOR=vim
 pip install flake8 tox
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
 git config --global user.email "chul@vmware.com"
@@ -9,8 +9,9 @@ git config --global user.name "Leo"
 
 cp -r .vim/ ~/
 cp .vimrc ~/
- [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd nsx-ujo
 find . -name "*.py" >> cscope.files
+find "nsx_ujo/bin" -name "nsx" >> cscope.files
 cscope -Rk
 
