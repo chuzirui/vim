34189997d7ad8107117060c87550afad19513f91
Leo Chu <chul@vmware.com>
2017-06-07 17:34:01 +0800

    no change to bashrc

--- dev-setup.sh
+++ dev-setup.sh
@@ -7,7 +7,7 @@ pip install flake8 tox
 export EDITOR=vim
 cp -r .vim/ ~/
 cp .vimrc.python ~/.vimrc
-cp .bashrc ~/
+#cp .bashrc ~/
  [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
