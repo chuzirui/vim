b73391ad3828b27388dd254a26fc56b7fe1bc878
Leo Chu <chul@vmware.com>
2017-06-15 05:19:32 +0000

    seoul

--- checkout.sh
+++ checkout.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 mkdir -p ~/.ssh
 cp id_rsa* ~/.ssh
+chmod 0400 ~/.ssh/id_rsa
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
 git config --global user.email "chul@vmware.com"
 git config --global user.name "Leo Chu"
 
 cd nsx-ujo
+pip install --upgrade pip
 pip install .
 find . -name "*.py" >> cscope.files
 find . -name "nsx" >> cscope.files
