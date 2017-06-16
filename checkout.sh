cad7d9a7d332cf52b6b50bbb89c81dbcf323bff4
Leo Chu <chul@vmware.com>
2017-06-16 09:40:23 +0000

    bashrc

--- checkout.sh
+++ checkout.sh
@@ -2,6 +2,8 @@
 mkdir -p ~/.ssh
 cp id_rsa* ~/.ssh
 chmod 0400 ~/.ssh/id_rsa
+cp .bashrc ~/
+source .bashrc
 git clone ssh://git@git.eng.vmware.com/nsx-ujo.git
 git config --global user.email "chul@vmware.com"
 git config --global user.name "Leo Chu"
