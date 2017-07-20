3b0906f5849d9cd402d8a2194faa22f5c5a3ff4b
leo chu <chuzirui@gmail.com>
2017-07-20 09:11:11 -0400

    lets enc

---
+++ cert.sh
@@ -0,0 +1,3 @@
+docker run --rm -t -i --net=host -v /etc/letsencrypt/:/etc/letsencrypt lea bash
+letsencrypt-auto renew
+
---
+++ instsho.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt update -y
+sudo apt install python python-pip -y
+sudo pip install shadowsocks
+a=`whereis ssserver | grep -o ' .*'`
+
+/usr/bin/python $a -p 8558 -k Log1tech -m aes-256-cfb --user nobody --workers 2 --log-file /var/log/ss.log -d start
