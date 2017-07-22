cea5f617ebdc808667dca0371fd31992a3f09188
leo chu <chuzirui@gmail.com>
2017-07-21 20:34:11 -0400

    path

--- ipt.sh
+++ ipt.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
 iptables -A INPUT  -s 49.5.6.114 -j REJECT --reject-with icmp-port-unreachable
+for a in $(grep 'can not parse header' /var/log/ss.log | egrep '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'   -o| sort|uniq);do iptables -A INPUT  -s $a -j REJECT --reject-with icmp-port-unreachable; done
+
 iptables -A INPUT  -s 103.77.56.144 -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT  -s 104.194.76.13 -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT  -s 49.4.171.225 -j REJECT --reject-with icmp-port-unreachable
