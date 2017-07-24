ef9d44b1e32ec37dcbd835d2fa58cf6fdfee453d
Leo Chu <chuzirui@gmail.com>
2017-07-24 10:48:30 +0900

    delete for duplicate

--- startss.sh
+++ startss.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+apt -y install python-pip
+pip install shadowsocks
 
 /usr/bin/python /usr/local/bin/ssserver -c /etc/shadowsocks.json --user nobody --workers 2 --log-file /var/log/ss.log -d start
 exit 0
