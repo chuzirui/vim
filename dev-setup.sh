8a6404d1f317d2be431314c035fb6f6da031184c
Leo Chu <chul@vmware.com>
2017-08-02 14:24:58 +0800

    yum

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 apt update -y
-apt install -y vim cscope xsel git-review exuberant-ctags
-apt install -y python-pip autojump thefuck
-apt install -y libssl-dev python-openssl
+apt install -y vim cscope xsel git-review exuberant-ctags | dnf install -y vim cscope xsel git-review ctags 
+apt install -y python-pip autojump thefuck | dnf -y install python-pip autojump thefuck
+apt install -y libssl-dev python-openssl | dnf -y install  openssl-devel
 apt-get -y install build-essential cmake python-dev python3-dev
+dnf -y install cmake python-devel python3-devel 
 pip install flake8 tox
 export EDITOR=vim
 cp -r .vim/ ~/
@@ -12,4 +13,5 @@ cp .vimrc.python ~/.vimrc
  [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
+yum -y install gcc-c++
 ./install.py --clang-completer
