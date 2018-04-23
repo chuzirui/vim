52796426772393c2e41f422f17664446e9d3a081
Leo Chu <chuzirui@gmail.com>
2018-04-23 21:34:52 +0000

    adding curl

--- dev-setup.sh
+++ dev-setup.sh
@@ -5,7 +5,7 @@ sudo apt install -y python-pip autojump software-properties-common
 mkdir -p ~/.pip
 cp pip.conf ~/.pip
 sudo pip install thefuck
-sudo apt install -y libssl-dev python-openssl silversearcher-ag
+sudo apt install -y libssl-dev python-openssl silversearcher-ag curl
 sudo apt-get -y install build-essential cmake python-dev python3-dev
 sudo dnf -y install cmake python-devel python3-devel
 sudo pip install flake8 tox
@@ -15,9 +15,6 @@ cp .vimrc.python ~/.vimrc
 #cp .bashrc ~/
  [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-cd ~/.vim/bundle/YouCompleteMe
-sudo yum -y install gcc-c++
-./install.py --clang-completer
 git remote set-url origin ssh://git@github.com/chuzirui/vim.git
 curl -o- -L https://raw.githubusercontent.com/TakeshiTseng/vim-language-p4/master/install.sh | bash
 git config --global user.email "chuzirui@gmail.com"
