5528eac71ad8403c565add908374325c288694f8
Leo Chu <chul@vmware.com>
2018-05-20 01:15:09 +0000

    sudo startss

--- instsho.sh
+++ instsho.sh
@@ -5,6 +5,6 @@ sudo apt install python python-pip -y
 pip install shadowsocks --user
 a=`whereis ssserver | grep -o ' .*'`
 
-/usr/bin/python $a -p 8558 -k Log1tech -m aes-256-cfb --user nobody --workers 2 --log-file /var/log/ss.log -d start
+sudo /usr/bin/python $a -p 8558 -k Log1tech -m aes-256-cfb --user nobody --workers 2 --log-file /var/log/ss.log -d start
 sslocal -s fugfw.com -p 8558 -l 8964 -k Log1tech -m aes-256-cfb
 
