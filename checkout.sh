9dfb000a0c9788f0d5c2d9d0db685013743abaa3
Leo Chu <chuzirui@gmail.com>
2018-07-01 17:46:08 +0800

    email changed

--- checkout.sh
+++ checkout.sh
@@ -4,7 +4,7 @@ cp id_rsa* ~/.ssh
 chmod 0400 ~/.ssh/id_rsa
 cp .bashrc ~/
 source .bashrc
-git config --global user.email "chul@vmware.com"
+git config --global user.email "chuzirui@gmail.com"
 git config --global user.name "Leo Chu"
 
 
