ae2936c91d8dfe4c3d0fccc0c9c56c60e58badc5
Leo Chu <chuzirui@gmail.com>
2018-07-26 12:59:37 +0800

    change proxy to ss

--- add-proxy.sh
+++ add-proxy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-ssh -D 8080 -fqN root@fugfw.com
+pip install shadowsocks
+#ssh -D 8080 -fqN root@fugfw.com
+sslocal -s 172.104.213.238 -p 8558 -l 8080 -k Qyff2011 -m aes-256-cfb &
+
 git config --global http.proxy 'socks5://127.0.0.1:8080'
 git config --global https.proxy 'socks5://127.0.0.1:8080'
 
