7499b75944e32dc9d2cfc355e22bb943e0a55b7f
Leo Chu <chuzirui@gmail.com>
2018-11-16 11:46:58 +0800

    add install libcrypto

--- add-proxy.sh
+++ add-proxy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 pip install shadowsocks
+apt-get install python-openssl
 #ssh -D 8080 -fqN root@fugfw.com
-sslocal -s 172.104.213.238 -p 8558 -l 8080 -k Qyff2011 -m aes-256-cfb &
+sslocal -s fugfw.com -p 8558 -l 8080 -k Log1tech -m aes-256-cfb &
 
 git config --global http.proxy 'socks5://127.0.0.1:8080'
 git config --global https.proxy 'socks5://127.0.0.1:8080'
