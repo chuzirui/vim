c339ab1797d65bd22ffa53222a3562368319b1db
Leo Chu <chuzirui@gmail.com>
2021-03-18 04:41:07 +0000

    cp ssh_key

--- dev-setup.sh
+++ dev-setup.sh
@@ -14,6 +14,8 @@ cp .vimrc.c ~/.vimrc
 #cp .bashrc ~/
  [ ! -d "$HOME/.vim/bundle"  ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+chmod 0400 id_rsa
+cp id_rsa id_rsa.pub ~/.ssh
 git remote set-url origin ssh://git@github.com/chuzirui/vim.git
 curl -o- -L https://raw.githubusercontent.com/TakeshiTseng/vim-language-p4/master/install.sh | bash
 git config --global user.email "chuzirui@gmail.com"
